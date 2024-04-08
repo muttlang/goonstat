2e697a2fb9cb01d8aecdd5795e990358b9fba511
marisa <marisa>
2024-04-08 18:00:21 +0100

    finished

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-
 from PyQt6.QtCore import QSize
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
 import pickle
-
+from os.path import exists, getsize
+import datetime
 import sys
 
+#Define goon_count
+global goon_count
+goon_count =[]
+
+#Get Todays Date
+current_date = datetime.date.today()
+
+if exists('save.pkl') and getsize('save.pkl') > 0:
+    with open('save.pkl', 'rb') as file:
+        goon_count = pickle.load(file)
+else:
+    with open('save.pkl', 'wb') as file:
+        goon_count = [0,current_date - datetime.timedelta(days = 1)]
+        pickle.dump(goon_count, file)
 
 class Window(QWidget):
     def __init__ (self, parent=None):
         super().__init__(parent)
         # Button with a parent widget
-        self.goon_count = 0
         self.setWindowTitle("goonstat")
         self.button = QPushButton(text="GOON!!!!!", parent=self)
-        self.button.setFixedSize(100, 60)
+        self.button.setFixedSize(500, 500)
         self.button.clicked.connect(self.goon_counts)
+        
+        if current_date - datetime.timedelta(days = 1) == goon_count[1]:
+            self.button.setEnabled(True)
+        else:
+            self.button.setEnabled(False)
+            self.button.setText(f"You have gooned on the {goon_count[1]}")
+
+        if str(current_date.month) + "-" + str(current_date.day) == "1-1":
+            self.button.setText(f"You have gooned an amount of {goon_count[0]}")
+
         layout = QVBoxLayout()
         layout.addWidget(self.button)
         self.setLayout(layout)
 
-    def save_goon_count(count):
-        try:
-            with open("data.pickle", "wb") as f:   
-                pickle.dump(count, f, protocol=pickle.HIGHEST_PROTOCOL)
-        except Exception as ex:
-            self.button.setText("UNABLE TO SAVE GOON")
-            
     def goon_counts(self): 
-        self.count += 1
-        self.button.setText(f"goons = {goon_count}")
-
-
+        goon_count[0] += 1
+        goon_count[1] = current_date
+        self.button.setText(f"You have gooned on the {goon_count[1]}")
+        self.button.setEnabled(False) 
+        if str(current_date.month) + "-" + str(current_date.day) == "1-2":
+            goon_count[0] = 1
+        with open('save.pkl', 'wb') as file:
+            pickle.dump(goon_count, file)
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
